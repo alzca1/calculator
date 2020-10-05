1c1e6875cee91e07807baa6c4eb4fe16730871e3
Álvaro Zájara <alzca1@gmail.com>
2020-10-05 11:51:32 +0200

    styling done

--- src/containers/calculator/keypad/Keypad.js
+++ src/containers/calculator/keypad/Keypad.js
@@ -8,11 +8,30 @@ class Keypad extends Component {
       <div className="Keypad">
         <div className="FunctionKeys">
           <div className="UpperKeys">
-            <Button label="AC" />
-            <Button label="+/-" />
-            <Button label="%" />
+            <Button className="UpperKey" label="AC" />
+            <Button className="UpperKey" label="+/-" />
+            <Button className="UpperKey" label="%" />
           </div>
-          <div className="DigitKeys"></div>
+          <div className="DigitKeys">
+            <Button className="DigitKey" className="key0" label="0" />
+            <Button className="DigitKey" label="." />
+            <Button className="DigitKey" label="1" />
+            <Button className="DigitKey" label="2" />
+            <Button className="DigitKey" label="3" />
+            <Button className="DigitKey" label="4" />
+            <Button className="DigitKey" label="5" />
+            <Button className="DigitKey" label="6" />
+            <Button className="DigitKey" label="7" />
+            <Button className="DigitKey" label="8" />
+            <Button className="DigitKey" label="9" />
+          </div>
+        </div>
+        <div className="OperatorKeys">
+          <Button className="OperatorKey" label="÷" />
+          <Button className="OperatorKey" label="x" />
+          <Button className="OperatorKey" label="-" />
+          <Button className="OperatorKey" label="+" />
+          <Button className="OperatorKey" label="=" />
         </div>
       </div>
     );
