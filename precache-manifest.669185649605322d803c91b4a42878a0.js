c0eee8ba25d772ebb2e78a14ee35f3297141321c
Álvaro Zájara <alzca1@gmail.com>
2020-10-07 13:07:56 +0200

    Updates

---
+++ precache-manifest.669185649605322d803c91b4a42878a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ab2ba1942402b8b26981bdda58b231",
+    "url": "/react-redux-calculator/index.html"
+  },
+  {
+    "revision": "5ca3bd12a5147264faf7",
+    "url": "/react-redux-calculator/static/css/main.eaa9f35c.chunk.css"
+  },
+  {
+    "revision": "821eb1db562c2624d1df",
+    "url": "/react-redux-calculator/static/js/2.89fa304b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-calculator/static/js/2.89fa304b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca3bd12a5147264faf7",
+    "url": "/react-redux-calculator/static/js/main.a67a97e2.chunk.js"
+  },
+  {
+    "revision": "04f83227ec25a8439d70",
+    "url": "/react-redux-calculator/static/js/runtime-main.8f521a5b.js"
+  }
+]);
